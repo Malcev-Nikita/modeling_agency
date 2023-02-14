9ea448443b7de3e573c83b2f65dfa1b446afcb3d
Malcev-Nikita <mnd1work@gmail.com>
2023-02-14 15:56:16 +0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import '../style/header.css';
+
+function Header() {
+    return (
+        <header className='header'>
+            <div className='container'>
+
+            </div>
+        </header>
+    );
+}
+
+export default Header;
