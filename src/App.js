7a91e0c3245e8690e44bb2920ee067b90153532d
Malcev-Nikita <mnd1work@gmail.com>
2023-02-15 09:04:33 +0400

    commit 15.2.23 (1)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Models from "./components/Models";
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Header/>
+      <Models />
     </div>
   );
 }
--- src/components/Models.js
+++ src/components/Models.js
@@ -1,13 +1,25 @@
 import '../style/models.css';
+import * as fs from 'node:fs/promises';
 
 function Models() {
     return (
         <section className='models'>
             <div className='container'>
-                
+                <div className='models_container'>
+                    
+                </div>
+
+                <button className='models_button'>Смотреть всех</button>
             </div>
         </section>
     );
+}
+
+function ModelsItem() {
+    const json = fs.readFileSync('../content.json');
+    const parse = JSON.parse(json);
+
+    console.log(parse);
 }
 
 export default Models;
